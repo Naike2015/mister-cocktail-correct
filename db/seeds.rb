1acd1e4a4f71ef2be4ba9a887e12c33502a16128
Naike Nembetwa <naikenembetwa@gmail.com>
2015-02-20 16:00:59 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,58 +6,83 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
- find_or_create_by('dry vermouth')
- find_or_create_by('gin')
- find_or_create_by('bourbon whisky')
- find_or_create_by('orange peel')
- find_or_create_by('syrup')
- find_or_create_by('white rum')
-
+ Ingredient.find_or_create_by(name: 'dry vermouth')
+ Ingredient.find_or_create_by(name: 'gin')
+ Ingredient.find_or_create_by(name: 'bourbon whisky')
+ Ingredient.find_or_create_by(name: 'orange peel')
+ Ingredient.find_or_create_by(name: 'syrup')
+ Ingredient.find_or_create_by(name: 'white rum')
+ Ingredient.find_or_create_by(name: 'lime juice')
+ Ingredient.find_or_create_by(name: 'strawberry schnapps')
+ Ingredient.find_or_create_by(name: 'light rum')
+ Ingredient.find_or_create_by(name: 'light juice')
+ Ingredient.find_or_create_by(name: 'strawberries')
+ Ingredient.find_or_create_by(name: 'tequila')
+ Ingredient.find_or_create_by(name: 'cointreau')
+ Ingredient.find_or_create_by(name: 'cognac')
+ Ingredient.find_or_create_by(name: 'triple sec')
+ Ingredient.find_or_create_by(name: 'lemon juice')
+ Ingredient.find_or_create_by(name: 'simple syrup')
+ Ingredient.find_or_create_by(name: 'worcestershire sauce')
+ Ingredient.find_or_create_by(name: 'tabasco')
+ Ingredient.find_or_create_by(name: 'salt')
+ Ingredient.find_or_create_by(name: 'pepper')
+ Ingredient.find_or_create_by(name: 'hot coffee')
+ Ingredient.find_or_create_by(name: 'Irish whiskey')
+ Ingredient.find_or_create_by(name: 'brown sugar')
+ Ingredient.find_or_create_by(name: 'fresh cream')
+ Ingredient.find_or_create_by(name: 'Apple jack')
+ Ingredient.find_or_create_by(name: 'gernadine')
+ Ingredient.find_or_create_by(name: 'gernadine')
 
 Cocktail.destroy_all
+Dose.destroy_all
 
 10.times do cocktail = Cocktail.create([
 {name: "Martini", doses: [
-  dose.create(description: "1 cl", ingredient: "Dry vermouth"),
-  dose.create(description: "5 cl", ingredient: "Gin")]},
+  Dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("Dry vermouth")),
+  Dose.create(description: "5 cl", ingredient: Ingredient.find_by_name("Gin"))]},
 {name: "Manhattan", doses: [
-  dose.create(description: "2 cl", ingredient: "vermouth"),
-  dose.create(description: "2 cl", ingredient: "bourbon whisky"),
-  dose.create(description: "2 oz", ingredient: "orange peel")]},
-{name:"Brooklyn", doses: [
-  dose.create(description: "1 cl", ingredient: "syrup"),
-  dose.create(description: "2 cl", ingredient: " white rum"),
-  dose.create(description: "1 cl", ingredient: "lime juice")]},
+  Dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("vermouth")),
+  Dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("bourbon whisky")),
+  Dose.create(description: "2 oz", ingredient: Ingredient.find_by_name("orange peel"))]},
+{name: "Brooklyn", doses: [
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("syrup")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name(" white rum")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("lime juice"))]},
 {name: "Daiquiri", doses: [
-  dose.create(description: "1 cl", ingredient: "strawberry schnapps"),
-  dose.create(description: "2 cl", ingredient: "light rum"),
-  dose.create(description: "2 cl", ingredient: "lime juice"),
-  dose.create(description: "2 cl", ingredient: "strawberries")]},
-{name:"Margarita", doses: [
-  dose.create(description: "2 cl", ingredient: "Tequila"),
-  dose.create(description: "2 cl", ingredient: "Cointreau"),
-  dose.create(description: "1 cl", ingredient: "Lime juice")},
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("strawberry schnapps")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("light rum")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("lime juice")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("strawberries"))]},
+{name: "Margarita", doses: [
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("tequila")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("cointreau")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("lime juice"))]},
 {name:"Sidecar", doses: [
-  dose.create(description: "2 cl", ingredient: "Cognac"),
-  dose.create(description: "1 cl", ingredient: "Triple Sec")
-  dose.create(description: "1 cl", ingredient: "Lemon juice")]},
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("cognac")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("triple sec")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("lemon juice"))]},
 {name: "French 75", doses: [
-  dose.create(description: "2 cl", ingredient: "Champagne"),
-  dose.create(description: "1 cl", ingredient: "Lemon juice",
-  dose.create(description: "1 cl", ingredient: "gin"),
-  dose.create(description: "2 cl", ingredient: "simple syrup")]},
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("champagne")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("lemon juice")),
+  dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("gin")),
+  dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("simple syrup"))]},
 {name: "Bloody Mary", doses: [
- dose.create (description: "dash", ingredient: "Worcestershire sauce"),
- dose.create (description: "dash", ingredient: "Tabasco"),
- dose.create (description: "dash", ingredient: "salt")
- dose.create (description: "dash", ingredipepper, ice cubes"},
-{name: "Irish Coffee", dose: " 3 oz Hot coffee, 2 parts Irish whiskey, 1 tablespoon brown sugar, 1 part fresh cream"},
-{name: "Jack Rose", dose: " 2 parts Apple jack, 1 part lemon, 2 cl Gernadine"}])
-
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("Worcestershire sauce")),
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("tabasco")),
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("salt")),
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("pepper")),
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("salt"))]},
+{name: "Irish Coffee", doses: [
+ dose.create(description: "dash", ingredient: Ingredient.find_by_name("pepper")),
+ dose.create(description: "3 oz", ingredient: Ingredient.find_by_name("hot coffee")),
+ dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("Irish whiskey")),
+ dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("brown sugar")),
+ dose.create(description: "1 cl", ingredient: Ingredient.find_by_name("fresh cream"))]},
+{name: "Jack Rose", doses: [
+ dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("Apple jack")),
+ dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("lemon juice")),
+ dose.create(description: "2 cl", ingredient: Ingredient.find_by_name("gernadine"))]}
+ ])
 end
-
-dose [
-
-]
-
-Ingredient.find_or_create_by
